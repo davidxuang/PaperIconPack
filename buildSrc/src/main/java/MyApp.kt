3bcd145cd0e38db8c4d226d9e57f549de96fb16b
David Huang <davidxuang@live.com>
2024-05-29 14:51:04 +0800

    Version 0.2.1

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.dvxg.paper"
-    const val version = 200
-    const val versionName = "0.2.0.95"
+    const val version = 201
+    const val versionName = "0.2.1.95"
 }
